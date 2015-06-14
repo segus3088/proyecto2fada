8e1eebdd37e823f7e4e915d95745162653745cba
SEBASTIAN SALAZAR <segus3088@gmail.com>
2015-06-13 23:32:30 -0500

    Agregando entrada por parametros

--- HorarioMonitores.java
+++ HorarioMonitores.java
@@ -2,7 +2,21 @@ public class HorarioMonitores {
 
 	public static void main(String argEntrada[]){
 
-	
+		if(argEntrada.length == 0){
+			System.out.println("No puede ser vacio");
+			System.exit(0);
+		}
+
+		if(argEntrada.length > 1){
+			System.out.println("Muchos argumentos");
+			System.exit(0);
+		}
+
+		String nombreArchivo = argEntrada[0];
+
+		System.out.println(nombreArchivo);
+
+		HorarioMonitores arrancar = new HorarioMonitores();	
 	}
 
 }
